86ee46ad62a16aef5b9273517d155139f7101276
Fasna <fasnaap7@gmail.com>
2026-02-22 13:27:03 +0530

    added user registration endpoint

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from fastapi import FastAPI, UploadFile, File
 import shutil
 import os
 from fastapi.middleware.cors import CORSMiddleware
-
-
 from inference.run_inference import run_inference
+from db_connection.database import engine, SessionLocal
+from db_connection import db_model
+from db_connection import schemas
+from db_connection.auth import create_access_token, hash_password, verify_password
+
+
+db_model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -22,6 +27,12 @@ app.add_middleware(
 )
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 UPLOAD_DIR = "uploaded_videos"
@@ -41,3 +52,27 @@ async def analyze_video(file: UploadFile = File(...)):
         "status": "success",
         "scores": scores
     }
+
+
+@app.post("/login")
+def login(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(db_model.User).filter(db_model.User.username == user.username).first()
+
+    if not db_user:
+        hashed_password = hash_password(user.password)
+        new_user = db_model.User(
+            username=user.username,
+            password=hashed_password
+        )
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        db_user = new_user 
+
+    if not db_user or not verify_password(user.password, db_user.password):
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    access_token = create_access_token(data={"sub": db_user.username})
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
